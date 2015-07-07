c25eff798b66af813979339edc6ee9090441c23c
petteriTeikari <petteri.teikari@gmail.com>
2015-07-07 16:09:11 -0400

    isosurfaces
    Point cloud initial estimation with isosurface

--- reconstructSegmentation.m
+++ reconstructSegmentation.m
@@ -22,10 +22,10 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
     % imported by a 3rd party software, you can use the export_stack_toDisk
     % which saves the stack as non-OME multilayer TIFF file (.tif),
     % converts to 16-bit and scales the maximum intensity value to 65,535
-    disp('Writing the segmented stack to disk as a TIFF file')
-    export_stack_toDisk(fullfile('figuresOut', 'segmentedStack.tif'), segmentation)
-    whos    
+    % disp('Writing the segmented stack to disk as a TIFF file')
+    % export_stack_toDisk(fullfile('figuresOut', 'segmentedStack.tif'), segmentation)
     
+    % whos   
     %       Name                  Size                 Bytes  Class     Attributes
     % 
     %   imageStack          256x256x4            2097152  double              
@@ -33,7 +33,8 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
     %   reconstruction      256x256x4            1048576  single              
     %   segmentation        256x256x4            1048576  single              
     
-    % quick'n'dirty ploy
+    % quick'n'dirty plot of the input
+    %{
     fig = figure('Color','w');
     
         % Maximum Intensity projections of the test stack
@@ -43,10 +44,29 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
         subplot(1,2,2)    
             imshow(max(segmentation,[],3),[])
             title('Segmented stack')
+    %}
 
+    %% EXTRACT THE POINT CLOUD
+    
+        % min-max values of the segmented data
+        minIn = min(segmentation(:)); maxIn = max(segmentation(:));
 
+        % exctract point cloud from the volumetric segmentation data
+        isovalue = 0.1 * maxIn;
+        [f,v] = isosurface(segmentation,isovalue);
+
+        % plot the vertices
+        patch('Faces',f,'Vertices',v, ...            
+                'edgecolor', 'none', ...
+                'facecolor', 'red');
+        view(-30,-54);
+        daspect([1,1,0.1])
+        view(3); axis tight
+        camlight 
+        lighting gouraud
+        xlabel('X'); ylabel('Y'); zlabel('Z')
+        title('Point Cloud with a lighting')
     
-    
-    
+  
     
    
