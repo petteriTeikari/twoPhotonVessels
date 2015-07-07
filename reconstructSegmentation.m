0b2dc70a1819d0fa9bbba5abb6ec0ae7b29d49ad
petteriTeikari <petteri.teikari@gmail.com>
2015-07-07 14:44:23 -0400

    TIFF write

--- reconstructSegmentation.m
+++ reconstructSegmentation.m
@@ -18,16 +18,33 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
     disp('3D Reconstruction (dummy)')
     reconstruction = segmentation;    
     
+    % if you just want to write the segmented version here to disk, and to
+    % imported by a 3rd party software, you can use the export_stack_toDisk
+    % which saves the stack as non-OME multilayer TIFF file (.tif),
+    % converts to 16-bit and scales the maximum intensity value to 65,535
+    disp('Writing the segmented stack to disk as a TIFF file')
+    export_stack_toDisk(fullfile('figuresOut', 'segmentedStack.tif'), segmentation)
+    whos    
+    
+    %       Name                  Size                 Bytes  Class     Attributes
+    % 
+    %   imageStack          256x256x4            2097152  double              
+    %   options               1x1                   3931  struct              
+    %   reconstruction      256x256x4            1048576  single              
+    %   segmentation        256x256x4            1048576  single              
+    
     % quick'n'dirty ploy
     fig = figure('Color','w');
     
-    % Maximum Intensity projections of the test stack
-    subplot(1,2,1)
-        imshow(max(imageStack,[],3),[])
-    subplot(1,2,2)    
-        imshow(max(segmentation,[],3),[])
-    
-    
+        % Maximum Intensity projections of the test stack
+        subplot(1,2,1)
+            imshow(max(imageStack,[],3),[])
+            title('Denoised stack (non-segmented')
+        subplot(1,2,2)    
+            imshow(max(segmentation,[],3),[])
+            title('Segmented stack')
+
+
     
     
     
