818faf844e0acfd67feaf37f46c8d657543104b8
petteriTeikari <petteri.teikari@gmail.com>
2015-07-07 17:25:40 -0400

    Marching Cubes
    Added 3rd party code for 3D reconstruction using Marching Cubes that is
    significantly faster at least compared to the "isosurface" from Matlab

--- reconstructSegmentation.m
+++ reconstructSegmentation.m
@@ -55,12 +55,12 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
         close all % close all open figures
         fig = figure('Color', 'w');
             scrsz = get(0,'ScreenSize'); % get screen size for plotting 
-            set(fig,  'Position', [0.03*scrsz(3) 0.045*scrsz(4) 0.35*scrsz(3) 0.90*scrsz(4)])
+            set(fig,  'Position', [0.03*scrsz(3) 0.045*scrsz(4) 0.9*scrsz(3) 0.50*scrsz(4)])
     
         sliceVector = 1:size(segmentation,3);
         numberOfContourLevelsPerSlice = 16;
         
-        subplot(2,1,1)
+        subplot(1,3,1)
         
         % this does not actually return anything, it just visualizes the
         % volumetric data
@@ -80,10 +80,15 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
         % extract from the volumetric segmentation data
         % http://www.mathworks.com/help/matlab/ref/isosurface.html
         isovalue = 0.1 * maxIn;
+        tic;
         [f,v] = isosurface(segmentation,isovalue);
+        time.isosurface = toc;
+        
+        isosurf_numberOfFaces = length(f);
+        isosurf_numberOfVertices = length(v);
 
         % plot the vertices
-        subplot(2,1,2)
+        subplot(1,3,2)
         patch('Faces',f,'Vertices',v, ...            
                 'edgecolor', 'none', ...
                 'facecolor', 'red');
@@ -93,8 +98,46 @@ function reconstruction = reconstructSegmentation(imageStack, segmentation, opti
             camlight 
             lighting gouraud
             xlabel('X'); ylabel('Y'); zlabel('Z')
-            title('Polygon mesh with a lighting')
+            titStr = sprintf('%s\n%s\n%s', ['"isosurface" (isovalue = ', num2str(isovalue,4), ') with a lighting'], ...
+                             ['no of faces = ', num2str(isosurf_numberOfFaces), ', no of vertices = ', num2str(isosurf_numberOfVertices)], ...
+                             ['computation time = ', num2str(time.isosurface,3), ' sec']);
+            title(titStr)
 
-  
+    %% EXTRACT THE MESH #2
     
-   
+        % Using Marching Cubes algorithm from Matlab FEX
+        % http://www.mathworks.com/matlabcentral/fileexchange/32506-marching-cubes
+        
+        % Note that even though the name of the algorithm is Marching
+        % Cubes, it can be implemented differently still in different
+        % programs, see for example Wiemann et al. (2015), 
+        % http://dx.doi.org/10.1007/s10846-014-0155-1
+        xgv = 1:size(segmentation,1);
+        ygv = 1:size(segmentation,2);
+        zgv = 1:size(segmentation,3);
+        [X,Y,Z] = meshgrid(xgv,ygv,zgv);        
+        
+        tic;
+        [F,V] = MarchingCubes(X,Y,Z,segmentation,isovalue);
+        time.marchingCubes = toc;
+        
+        cubes_numberOfFaces = length(F);
+        cubes_numberOfVertices = length(V);
+
+        subplot(1,3,3)
+        patch('Faces',F,'Vertices', V, ...            
+                'edgecolor', 'none', ...
+                'facecolor', 'red');
+            
+            view(34,-38);
+            daspect([1,1,0.1]); axis tight
+            camlight 
+            lighting gouraud
+            xlabel('X'); ylabel('Y'); zlabel('Z')
+            titStr2 = sprintf('%s\n%s\n%s', ['"Marching Cubes" (isovalue = ', num2str(isovalue,4), ') with a lighting'], ...
+                             ['no of faces = ', num2str(cubes_numberOfFaces), ', no of vertices = ', num2str(cubes_numberOfVertices)], ...
+                             ['computation time = ', num2str(time.marchingCubes,3), ' sec']);
+            title(titStr2)
+    
+        % save the figure
+        export_fig(fullfile('figuresOut', 'reconstructionTesting.png'), '-r300', '-a1')
