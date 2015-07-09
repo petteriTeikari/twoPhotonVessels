ccd07ee71a13b5b8d9627890403566438df67c07
petteriTeikari <petteri.teikari@gmail.com>
2015-07-09 13:03:43 -0400

    autofluorescence refs

--- 2PFM_spectralAnalysis/import_fluorophoreData.m
+++ 2PFM_spectralAnalysis/import_fluorophoreData.m
@@ -1,8 +1,10 @@
 function fluoro = import_fluorophoreData()
 
+    %% FLUORESCENT MARKERS
+
         % OGB
         % https://www.lifetechnologies.com/order/catalog/product/O6807
-        tmpOGB = importdata(fullfile('data','Oregon Green 488 BAPTA.csv'), ',', 1);
+        tmpOGB = importdata(fullfile('data','OregonGreen488_BAPTA.csv'), ',', 1);
             fluoro{1}.wavelength = tmpOGB.data(:,1);
             fluoro{1}.wavelengthRes = fluoro{1}.wavelength(2) - fluoro{1}.wavelength(1);
             fluoro{1}.excitation = tmpOGB.data(:,2);
@@ -26,3 +28,27 @@ function fluoro = import_fluorophoreData()
             fluoro{2}.emission = interp1(tmpSR101ems.data(:,1), tmpSR101ems.data(:,2), fluoro{2}.wavelength);
             fluoro{2}.name = 'SR-101';
             fluoro{2}.plotColor = [1 0 0];
+
+    %% AUTOFLUORESCENCE
+    
+        % How much spectral cross-talk from autofluorescence?
+    
+        % Astroglial autofluorescence, e.g. Fig. 2 of Oheim et al. (2014)
+        % http://dx.doi.org/10.1016/j.bbamcr.2014.03.010
+        
+        % NADH
+        
+        % Baraghis E, Devor A, Fang Q, Srinivasan VJ, Wu W, Lesage F, Ayata C, Kasischke KA, Boas DA, Sakadžić S. 2011. 
+        % Two-photon microscopy of cortical NADH fluorescence intensity changes: correcting contamination from the hemodynamic response. 
+        % J. Biomed. Opt 16:106003–106003–13. 
+        % http://dx.doi.org/10.1117/1.3633339.
+        
+        % Pu Y, Sordillo LA, Alfano RR. 2015. 
+        % Nonnegative constraint analysis of key fluorophores within human breast cancer using 
+        % native fluorescence spectroscopy excited by selective wavelength of 300 nm. 
+        % In: Vol. 9318, p. 93180V–93180V–11. 
+        % http://dx.doi.org/10.1117/12.2076102.
+
+        
+        
+        
