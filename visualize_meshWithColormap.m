369d81898663c437311c95e3e04597b3eca7d9b2
petteriTeikari <petteri.teikari@gmail.com>
2015-08-17 16:46:15 -0400

    missing seed for mesh colormap

---
+++ visualize_meshWithColormap.m
@@ -0,0 +1,33 @@
+function visualize_meshWithColormap(faces, vertices, colormap, plotType, options)
+
+    %% CHECK INPUTS
+    
+        % we can later add some specific things here depending on what
+        % values we are using as the colormap of the mesh
+        if strcmp(plotType, 'SDF')
+
+        elseif strcmp(plotType, 'change')
+
+        else
+
+        end
+
+    %% VISUALIZE
+    
+        % code here
+    
+    
+        
+        
+    %% SAVE TO DISK 
+    
+        % export code here
+        
+        % if you figure out how to export .ply files with the color, it
+        % would be great
+        
+        % e.g. 
+        % http://stackoverflow.com/questions/16666308/saving-kinect-reconstructed-3d-view-into-a-file
+        % https://social.msdn.microsoft.com/Forums/en-US/88cff0eb-e794-4541-8786-183cda5fd398/kinect-fusion-head-scanning-obj-export-no-texture-map?forum=kinectsdk
+            % "Fusion color is done by way of Vertex colors. OBJ doesn't
+            % support vertex color, so use the new PLY export option."
