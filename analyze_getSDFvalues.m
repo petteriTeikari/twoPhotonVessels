43b1beab4a2bff71f27c4429c87ef6fbcceade17
petteriTeikari <petteri.teikari@gmail.com>
2015-08-31 12:18:19 -0400

    fix variables
    removed some hardcoded paths, and added error-checking for most likely
    malfunctioning CGAL filter

--- analyze_getSDFvalues.m
+++ analyze_getSDFvalues.m
@@ -1,36 +1,54 @@
  % Uses command-line CGAL implementation
 function diameterVals = analyze_getSDFvalues(fileNameForMesh, options)
 
+    if nargin == 2
+        disp('"options" is a lazy placeholder if we need to pass some parameters eventually')
+    elseif nargin == 1
+        options = [];
+    end
+
     % SHAPE DIAMETER FUNCTION (SDF)
+    % the full path to the .off file is save in "reconstructMeshFromSegmentation.m"    
+    % or you can use whatever .off file that you wish
+    if isunix || ismac
+        fileSavePathCell = strsplit(fileNameForMesh, '/');
+    else
+        fileSavePathCell = strsplit(fileNameForMesh, '\');
+    end    
+    fileSavePath = fileSavePathCell{end};
+    fileSavelocation = strrep(fileNameForMesh, fileSavePath, '');     
 
-    % the full path to the .off file is save in "reconstructMeshFromSegmentation.m"
+    % Getting SDF Vals
+    command = [fullfile('.', 'CGAL', 'SDFRetrival', 'build', 'PropertyVals'), ' ', fileNameForMesh,  ' ', fileSavelocation];
     
+    % the call on terminal to compute the SDFs
+    [status, cmdout] = system(command);    
+    check_CGALSystemOutput(cmdout, status, 'SDF')   
     
-    fileSavePathCell = strsplit(fileNameForMesh, '/');
-    fileSavePath = fileSavePathCell{end}
-    fileSavelocation = strrep(fileNameForMesh, fileSavePath, '');
-     
+    % read the values then from disk back to Matlab
+    txtFilepath= fullfile(fileSavelocation, 'SDFVals.txt');
+    fileID = fopen(txtFilepath,'r');
+    formatSpec = '%f';
+    diameterVals = fscanf(fileID,formatSpec);
+    fclose(fileID);
+    
+end
 
-    % Getting SDF Vals 
-%     try 
-%         command = ['./PropertyVals ' [path, 'out', [options.reconstructFileNameOut, '.off']] ' ' fileSavelocation]
-%     whos
-%     catch err
-%         err
-%         err.message
-%         error('Petteri: Some error with the command definiton?')
-%     end
+function check_CGALSystemOutput(cmdout, status, filterName)
 
-% command = ['./PropertyVals ' [path, 'out', [options.reconstructFileNameOut, '.off']] ' ' fileSavelocation]
-
-command = ['./PropertyVals /home/highschoolintern/Desktop/TestReconstruction2/testReconstruction_4slicesPhysical_reconstruction_isolatedRemoved_decimated.off ' fileSavelocation];
-whos
-
-[status, cmdout] = system(command);
-disp(cmdout); 
-txtFilepath= [fileSavelocation '/SDFVals.txt'];
-fileID = fopen(txtFilepath,'r');
-formatSpec = '%f';
-diameterVals = fscanf(fileID,formatSpec);
-fclose(fileID);
+    % Check that everything went ok
+    if strfind(cmdout, 'error while loading shared libraries')
+        if strfind(cmdout, 'libCGAL')
+            cmdout
+            error('CGAL Library not found! Did you build from the source yourself?')
+        else
+            cmdout
+            error('here2')
+        end
+    elseif strfind(cmdout, 'No such file or directory')
+        cmdout
+        error(['Folder not found, have you built the ',  filterName, ' filter already?'])
+    else
+        cmdout
+    end
 end
