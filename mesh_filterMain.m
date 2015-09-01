f496507167ee2b18e53ae5d875675716428056a2
petteriTeikari <petteri.teikari@gmail.com>
2015-09-01 13:21:34 -0400

    filter update

--- mesh_filterMain.m
+++ mesh_filterMain.m
@@ -1,44 +1,80 @@
 function reconstructionOut = mesh_filterMain(reconstruction, operation, algorithm, options, t, ch)
+    
+    %% INPUT CHECK
 
-    disp('    -- mesh filter dummy')
-    reconstructionOut = reconstruction;
+        % debug
+        if nargin == 0
+
+            % get the current path (where this .m file is)
+            fileName = mfilename; fullPath = mfilename('fullpath');
+            pathCode = strrep(fullPath, fileName, '');
+            if ~isempty(pathCode); cd(pathCode); end
+
+            load(fullfile('testData', 'testMeshFilter.mat'))
+            operation = 'simplification'; % sequential, on top of previous
+            algorithm = 'CGALcombo';
+            options = [];
+            t = 1;
+            ch = 1;
+        else
+            % save(fullfile('testData', 'testMeshFilter.mat'), 'reconstruction')
+        end
+        reconstructionOut = reconstruction;
     
-    if strcmp(operation, 'repair')
+    %% FILTERING
     
-        % repair something
-        if strcmp(algorithm, 'Basic')
-            disp('Some "basic" repair functions to automatically fix the mesh, like in Meshlab')
-            
+        if strcmp(operation, 'repair')
+
+            % repair something
+            if strcmp(algorithm, 'Basic')
+                disp('Some "basic" repair functions to automatically fix the mesh, like in Meshlab')
+
+            else
+                error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
+            end
+
+        elseif strcmp(operation, 'simplification')
+
+            % do something, e.g. WLOP
+            if strcmp(algorithm, 'WLOP')
+                disp('CGAL WLOP call here')
+
+            elseif strcmp(algorithm, 'Outlier removal')
+               disp('CGAL Outlier Removal call here')
+
+            elseif strcmp(algorithm, 'CGALcombo')
+                disp('CGAL Outlier Removal call here')
+                param = points_setCGALcomboDefaultParam();
+                pointsOut = points_filterCGALCombo(reconstruction, param);
+               
+            else
+                error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
+            end
+
+
+        elseif strcmp(operation, 'smoothing')
+
+            % do something else, e.g. BILATERAL SMOOTHING
+            if strcmp(algorithm, 'Bilateral')
+                disp('CGAL Bilateral Smoothing  call here')
+
+            else
+                error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
+            end
+
         else
-            error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
+
+            error('this kind of doing not implemented')
+
         end
         
-    elseif strcmp(operation, 'simplification')
         
-        % do something, e.g. WLOP
-        if strcmp(algorithm, 'WLOP')
-            disp('CGAL WLOP call here')
-            
-        elseif strcmp(algorithm, 'Outlier removal')
-           disp('CGAL Outlier Removal call here')
-            
-        else
-            error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
-        end
+    
+        
+        
+              
             
         
-    elseif strcmp(operation, 'smoothing')
-        
-        % do something else, e.g. BILATERAL SMOOTHING
-        if strcmp(algorithm, 'Bilateral')
-            disp('CGAL Bilateral Smoothing call here')
             
-        else
-            error(['You wanted algorithm: "', algorithm, '" which is not implemented'])
-        end
-       
-    else
+            
         
-        error('this kind of doing not implemented')
-        
-    end
